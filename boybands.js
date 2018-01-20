6867a7ab1d2ac29eb75848900adc2c544d6044fb
Alicia Zaludova <aliciazaludova@gmail.com>
2018-01-20 14:14:49 -0600

    started on a for loop

---
+++ boybands.js
@@ -0,0 +1,13 @@
+let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+const bandElement = document.getElementById("boy-bands");
+console.log(bandElement);
+
+const veggieElement = document.getElementById("vegetables");
+
+//executing a loop:
+
+for (let looptracker = 0; looptracker < 5; looptracker += 1) {
+
+}
